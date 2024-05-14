8d777c9d5cba786931855044bdc223ed395a68d0
Maeda jo <zeal23410074@fsgcl.jp>
2024-05-15 00:04:28 +0900

    3.4 塗りつぶした楕円を描く

--- 初級Java2.5/src/start.java
+++ 初級Java2.5/src/start.java
@@ -10,7 +10,7 @@ public class start {
 
 		MyFrame frame2 = new MyFrame();
 		frame2.setLocation(350,200);
-         */
+
 
 		int x =2;
 		int y =3;
@@ -21,6 +21,15 @@ public class start {
 
         MyFrame frame4 = new RectFrame();
         frame4.setLocation(250,300);
+
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectTest();
+		//frame1.fillRect(30,50,200,100);
+
+         */
+
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new OvalTest();
 	}
 
 }
