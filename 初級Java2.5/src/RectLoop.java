bdf8d4aeb457366fd474b209366ea12a907ddaef
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-20 11:10:29 +0900

    確認問題4-2

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -2,7 +2,7 @@ public class RectLoop extends MyFrame {
     public void run() {
         setColor(0,128,0);
         int x = 30;
-        while(x <= 170) {
+        while(x <= 220) {
             fillRect(x,80,10,100);
             x+=20;
         }
