03a227320fe92f07c33f3a50a5774804c29fe55b
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-20 09:49:28 +0900

    3.6 文字列を書く

--- 初級Java2.5/src/OvalTest.java
+++ 初級Java2.5/src/OvalTest.java
@@ -2,6 +2,6 @@ public class OvalTest extends MyFrame{
     public void run() {
         fillOval(30,40,50,100);
         fillOval(130,40,50,100);
-        fillOval(30,200,150,10);
+        fillRect(30,200,150,10);
     }
 }
---
+++ 初級Java2.5/src/TextTest.java
@@ -0,0 +1,7 @@
+public class TextTest extends MyFrame{
+    @Override
+    public void run() {
+        drawString("おはよう",100,100,15);
+        drawString("こんにちは",100,200,20);
+    }
+}
