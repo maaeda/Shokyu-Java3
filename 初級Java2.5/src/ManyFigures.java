940450996d3c9db06c1abf2334b4bfae3ff1d3c7
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-20 10:52:59 +0900

    4-1 図形をたくさん描く

---
+++ 初級Java2.5/src/ManyFigures.java
@@ -0,0 +1,18 @@
+public class ManyFigures extends MyFrame{
+    public void run() {
+        /*
+        setColor(0,128,0);
+        fillRect(30,80,10,100);
+        fillRect(50,80,10,100);
+        fillRect(70,80,10,100);
+        fillRect(90,80,10,100);
+        fillRect(110,80,10,100);
+        fillRect(130,80,10,100);
+        fillRect(150,80,10,100);
+        fillRect(170,80,10,100);
+        */
+        int x = 30;
+        setColor(0,128,0);
+        fillRect(x,80,10,100);
+    }
+}
--- 初級Java2.5/src/start.java
+++ 初級Java2.5/src/start.java
@@ -29,7 +29,7 @@ public class start {
          */
 
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new TextTest();
+		MyFrame frame1 = new ManyFigures();
 	}
 
 }
