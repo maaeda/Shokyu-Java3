b950fdce7cf489b95e1544b77c601d5b5fadf733
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-21 12:18:18 +0900

    5-2 図形を動かす（アニメーション）

--- 初級Java2.5/src/DisplayRectsSlow.java
+++ 初級Java2.5/src/DisplayRectsSlow.java
@@ -3,9 +3,13 @@ public class DisplayRectsSlow extends MyFrame{
         setColor(0,128,0);
         int x = 30;
         while(x <= 170) {
+            //画面を消す
+            clear();
+
+            //図形を表示する
             fillRect(x,80,10,100);
-            x+=20;
-            sleep(1);
+            x+=5;
+            sleep(0.016);
         }
     }
 }
