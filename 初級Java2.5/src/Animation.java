f0c06d763a5699e4d2cb2d3953a8526d2398d7f3
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-27 11:54:59 +0900

    おまけ１

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,10 +1,11 @@
 public class Animation extends MyFrame{
     public void run() {
         int y = 100;
+        int crry =y;
         int x = 30;
-        int cnt = 0;
-        while(cnt < 3) {
-            while (x <= 170) {
+        int cnt = 2;
+        while(true) {
+            while (x <= 200) {
                 //画面を消す
                 clear();
                 //図形を表示する
@@ -13,6 +14,16 @@ public class Animation extends MyFrame{
                 x += 5;
                 sleep(0.016);
             }
+            while (y <= crry+100) {
+                //画面を消す
+                clear();
+                //図形を表示する
+                setColor(0, 128, 0);
+                fillRect(x, y, 10, 100);
+                y += 5;
+                sleep(0.016);
+            }
+            crry+=100;
             while (x >= 30) {
                 //画面を消す
                 clear();
@@ -22,7 +33,16 @@ public class Animation extends MyFrame{
                 x -= 5;
                 sleep(0.016);
             }
-            cnt++;
+            while (y <= crry+100) {
+                //画面を消す
+                clear();
+                //図形を表示する
+                setColor(0, 128, 0);
+                fillRect(x, y, 10, 100);
+                y += 5;
+                sleep(0.016);
+            }
+            crry+=100;
         }
     }
 }
