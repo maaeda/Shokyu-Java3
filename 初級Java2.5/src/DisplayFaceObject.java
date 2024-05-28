8acd9363e9d0be136193dd3d61a2681e6b7d062d
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-05-28 10:12:17 +0900

    練習問題6-2

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -3,7 +3,10 @@ public class DisplayFaceObject extends MyFrame{
         Face face1 = new Face(50,50);
         face1.draw(this);
 
-        Face face2 = new Face(200,100);
-        face1.draw(this);
+        Face face2 = new Face(250,50);
+        face2.draw(this);
+
+        Face face3 = new Face(150,150);
+        face3.draw(this);
     }
 }
