3f63f44f7e9805013246c5532a1027896430608e
Maeda jo <133735669+maaeda@users.noreply.github.com>
2024-06-10 23:44:19 +0900

    練習問題8-1

--- 初級Java2.5/src/DisplayCarsTrains.java
+++ 初級Java2.5/src/DisplayCarsTrains.java
@@ -2,10 +2,14 @@ import java.util.Vector;
 
 public class DisplayCarsTrains extends MyFrame{
     public void run(){
-        Vehicle[] vehicles = new Vehicle[3];
+        Vehicle[] vehicles = new Vehicle[6];
         vehicles[0] = new Car(10,30,3,0);
-        vehicles[1] = new Car(10,100,5,0);
+        vehicles[1] = new Car(100,100,-5,0);
         vehicles[2] = new Train(10,150,5,0);
+        vehicles[3] = new Train(100,200,-5,0);
+        vehicles[4] = new Truck(10,250,5,0);
+        vehicles[5] = new Truck(100,300,-5,0);
+
 
         for (int i = 0;i<30;i++){
             clear();
